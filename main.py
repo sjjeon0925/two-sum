523d3f226d6dfe7be05697ceb69202b85c7d74bf
sjjeon0925 <115344621+sjjeon0925@users.noreply.github.com>
2023-10-04 19:38:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,4 @@ def twoSum(self, nums: List[int], target: int) -> List[int]:
     for i in range(len(nums)):
       for j in range(i+1,len(nums)):
         if nums[i] + nums[j] == target:
-          print('[',i,',',j,']',sep='')
-    return
+          return [i,j]
